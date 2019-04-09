5831dc51e88b47383257dd9f21263e389a092f34
Derrick Dieso <derrickjdieso@gmail.com>
2019-04-09 14:46:22 -0400

    added api hit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+  componentWillMount() {
+      this.callAPI();
+  }
   render() {
     return (
       <div className="App">
@@ -20,6 +33,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p className="App-intro">{this.state.apiResponse}</p>
       </div>
     );
   }
