4bf59aa23073dea154484372bb0091bb45d3889b
Derrick Dieso <derrickjdieso@gmail.com>
2019-04-10 16:35:48 -0400

    Sample Counter

--- test.jsx
+++ test.jsx
@@ -1,39 +1,40 @@
-class MyComponent extends React.Component {
+class Counter extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        visibility: false
+        count: 0
       };
       // change code below this line
-      this.toggleVisibility = this.toggleVisibility.bind(this)
+      this.increment = this.increment.bind(this)
+      this.reset = this.reset.bind(this)
+      this.decrement = this.decrement.bind(this)
       // change code above this line
     }
     // change code below this line
-      toggleVisibility() {
-      if (this.state.visibility == true) {
+    increment(){
       this.setState({
-        visibility: false
-      });} else {
-        this.setState({
-          visibility: true
-        })
-      }
-      }
+        count: this.state.count + 1
+      });
+    }
+    decrement(){
+      this.setState({
+        count: this.state.count - 1
+      });
+    }
+    reset(){
+      this.setState({
+        count: 0
+      });
+    }
     // change code above this line
     render() {
-      if (this.state.visibility) {
-        return (
-          <div>
-            <button onClick={this.toggleVisibility}>Click Me</button>
-            <h1>Now you see me!</h1>
-          </div>
-        );
-      } else {
-        return (
-          <div>
-            <button onClick={this.toggleVisibility}>Click Me</button>
-          </div>
-        );
-      }
+      return (
+        <div>
+          <button className='inc' onClick={this.increment}>Increment!</button>
+          <button className='dec' onClick={this.decrement}>Decrement!</button>
+          <button className='reset' onClick={this.reset}>Reset</button>
+          <h1>Current Count: {this.state.count}</h1>
+        </div>
+      );
     }
   };
