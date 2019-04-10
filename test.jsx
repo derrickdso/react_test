b7990e874860c60a41090c9581c8c13b70c85881
Derrick Dieso <derrickjdieso@gmail.com>
2019-04-10 14:49:48 -0400

    cont. stateful testing

--- test.jsx
+++ test.jsx
@@ -1,15 +1,16 @@
-
-class StatefulComponent extends React.Component {
+class MyComponent extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {name:"wow"}
-      // initialize state here
-  
+      this.state = {
+        name: 'freeCodeCamp'
+      }
     }
     render() {
       return (
         <div>
+          { /* change code below this line */ }
           <h1>{this.state.name}</h1>
+          { /* change code above this line */ }
         </div>
       );
     }
