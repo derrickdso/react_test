57277b56f1f5a63824d019addda28f924937d088
Derrick Dieso <derrickjdieso@gmail.com>
2019-04-10 14:51:25 -0400

    Stateful const rendering

--- test.jsx
+++ test.jsx
@@ -6,10 +6,13 @@ class MyComponent extends React.Component {
       }
     }
     render() {
+      // change code below this line
+      const name = this.state.name
+      // change code above this line
       return (
         <div>
           { /* change code below this line */ }
-          <h1>{this.state.name}</h1>
+          <h1>{name}</h1>
           { /* change code above this line */ }
         </div>
       );
